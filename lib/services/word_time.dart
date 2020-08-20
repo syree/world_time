45fae888e5fc03526a79ff48e0c452e31d8a7d4e
syree <harshchaudharyp1cleaner@gmail.com>
2020-08-21 02:06:22 +0530

    Completed the project and uploading for the first time.

---
+++ lib/services/word_time.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+  String location; // location name for the UI
+  String time; // time in that location
+  String flag; //url to an asset flag icon
+  String url; // location url for api endpoint
+  bool isDaytime; // true or false if daytime or not
+  WorldTime({this.location,this.flag,this.url});
+  Future<void> getTime() async {
+
+    try{
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
+      //create date time object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      //set the time property
+      isDaytime = now.hour>6 && now.hour < 20? true : false;
+      time = DateFormat.jm().format(now);
+    }
+    catch(e){
+      print('Caught error : $e');
+      time = 'Could not get time data';
+    }
+
+  }
+}
